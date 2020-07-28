384f0fc431c6ae18971f3c520357607ad161d7c1
Tejasvi Vig <tejasvivig09@gmail.com>
2020-07-28 15:59:24 -0300

    Handling User functions

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require ('http');
+
+const {addUser, removeUser, getUsers, getUserInRoom } = require('./users.js');
+
 const router = require("./router");
 
 const PORT = process.env.PORT || 5000;
@@ -12,7 +15,17 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!!!');
+    socket.on('join', ({name,room}, callback) => {
+        const {user,error} = addUser({id:socket.id,name,room});
+        if(error){
+            return callback(error);
+        }
+
+        socket.emit('message', {user: 'admin' ,text: `${user.name}, welcome to the room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} , has joined!`}); 
+        socket.join(user.room);
+        callback();
+    });
 
     socket.on('disconnect', () => {
         console.log('User has left!!!');
